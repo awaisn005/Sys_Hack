293af62268d60e95e0f1b0d84b0301a3c8702229
awaisn005 <awaisnaseer005@gmail.com>
2024-05-20 00:40:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 
 setup(
-    name='mac-changer',
+    name='sys_Hack',
     version='1.0',
-    packages=['mac_changer'],
+    packages=['sys_Hack'],
     install_requires=[],
     author='Awais Naseer',
-    description='A tool to change the MAC address of a network interface',
-    url='https://github.com/awaisn005/mac-changer',
+    description='Backdoor Access to OS',
+    url='https://github.com/awaisn005/sys_Hack',
     classifiers=[
         'Programming Language :: Python :: 3',
     ],
     entry_points={
         'console_scripts': [
-            'mac-changer=mac_changer:main',
+            'sys-Hack=sys_Hack:main',
         ],
     },
 )
